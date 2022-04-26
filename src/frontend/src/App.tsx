551a6b281d56151d147080a585671e924c9caa31
Zeus-s <kristabdixx@gmail.com>
2022-04-26 21:30:50 +0700

    add hero section

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import {
-  BrowserRouter, Route, Routes
+  BrowserRouter, Link, Route, Routes
 } from "react-router-dom";
 import './App.css';
 import Navigation from './components/Navigation';
 import AddDesease from './pages/AddDesease';
 import DNATest from './pages/DNATest';
 import Search from './pages/Search';
+import banner from './banner.svg';
 
 function App() {
   return (
@@ -15,11 +16,14 @@ function App() {
       <BrowserRouter>
         <Navigation/>
         <Routes>
-          <Route path="/" element={<DNATest/>} />
+          <Route path="/" element={<img src={banner} alt="banner" className='banner'/>} />
+          <Route path="/test" element={<DNATest/>} />
           <Route path="/add" element={<AddDesease />} />
           <Route path="/search" element={<Search />} />
         </Routes>
+        
       </BrowserRouter>
+      
     </div>
   );
 }
--- src/frontend/src/components/Navigation.tsx
+++ src/frontend/src/components/Navigation.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from './../logo.svg';
+import logo from './../logo2.svg';
 
 function Navigation() {
   return (
     <Navbar className='navbar d-flex justify-content-between px-4'>
       <Link to="/">
-        <img src={logo} alt="logo" className='w-50'/>
+        <img src={logo} alt="logo" className='logo'/>
       </Link>
       <div>
-        <Link to="/" className='mx-4 nav-item'>DNA Test</Link>
+        <Link to="/test" className='mx-4 nav-item'>DNA Test</Link>
         <Link to="/search" className='mx-4 nav-item'>Search</Link>
         <Link to="/add" className='mx-4 nav-item'>Add Disease</Link>
       </div>
