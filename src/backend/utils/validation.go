8b26351d12a9109c686da62b4a23b3f29b261330
Zeus-s <kristabdixx@gmail.com>
2022-04-27 22:55:18 +0700

    move regex validation to backend

--- src/backend/utils/validation.go
+++ src/backend/utils/validation.go
@@ -5,9 +5,19 @@ import (
 )
 
 var dnaRegex *regexp.Regexp = regexp.MustCompile("^[ATCG]+$")
+var searchRegex *regexp.Regexp = regexp.MustCompile(`/^\s*\d{2}\s+\w+\s+\d{4}\s+\w+\s*$/g`)
+var diseaseRegex *regexp.Regexp = regexp.MustCompile(`/^\s*\w+\s*$/g`)
 
 func IsValidDNA(s string) bool {
 	return dnaRegex.MatchString(s)
+}
+
+func IsValidInput(s string) bool {
+	return searchRegex.MatchString(s)
+}
+
+func IsValidDiseaseSearchInput(s string) bool {
+	return diseaseRegex.MatchString(s)
 }
 
 func GetIdx(sequencechar byte) int {
